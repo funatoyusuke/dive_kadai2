47ffca23bca42e63fe67eb8f42f11b0f87a84567
funato <funato1990@yahoo.co.jp>
2018-11-24 08:29:35 +0000

    デプロイ1

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
-    validates :title,:content,presence: true
+    validates :title,:content,presence: true, length: { in: 1..140 }
     validates :image, presence: true
     has_many :favorites, dependent: :destroy
     has_many :favorite_users, through: :favorites, source: :user
